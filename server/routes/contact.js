f0198767945783d11bff6a238728ebee885ca0f9
Wonsuk Cha <wonscha@outlook.com>
2021-02-27 22:11:24 +0900

    Implement CRUD for Business Contact List

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -12,6 +12,7 @@ let router = express.Router();
 
 let Contact = require('../models/contact');
 
+/* Get Route for displaying Contact List page */
 router.get('/', (req, res, next) => {
     Contact.find((err, contactList) => {
         if (err) {
@@ -31,7 +32,58 @@ router.get('/', (req, res, next) => {
             });
         }
     })
+});
 
+/* GET Route for displaying Contact Edit page */
+router.get('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    Contact.findById(id, (err, contactToEdit) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        } else {
+            res.render('contact/edit', {
+                title: "Business Contact Edit",
+                contactToEdit: contactToEdit
+            })
+        }
+    })
+});
+
+/* POST Route for processing Contact Edit page */
+router.post('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    let updateContact = Contact({
+        _id: id,
+        name: req.body.name,
+        number: req.body.number,
+        email: req.body.email
+    });
+
+    Contact.updateOne({ _id: id }, updateContact, (err) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        } else {
+            res.redirect('/contact-list');
+        }
+    })
+});
+
+/* GET to perform deleting Contact */
+router.get('/delete/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    Contact.deleteOne({ _id: id }, (err) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        } else {
+            res.redirect('/contact-list');
+        }
+    })
 })
 
 module.exports = router;
